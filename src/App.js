7f1cdb380a238b72c747ceb28f7312abd45e4a7b
nguyentien87xd <nguyentien87xd@gmail.com>
2023-11-08 17:17:31 +0700

    Update FE (LoginForm & RegisterForm)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import Header from "./components/common/Header";
-import {Route, Routes} from "react-router-dom";
 import Footer from "./components/common/Footer";
-// import { Counter } from './feature/counter/Counter';
+import {Route, Router, Routes, Switch} from "react-router-dom";
+import React from "react";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <Routes>
-        </Routes>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Header/>
+            <Routes>
+
+                <Route path="/login" element={<LoginPage/>}></Route>
+                <Route path="/register" element={<RegisterPage/>}></Route>
+
+            </Routes>
+            <Footer/>
+
+        </div>
+    );
 }
 
 export default App;
