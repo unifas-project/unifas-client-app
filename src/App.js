1280d526dcc5505c5a583d9f50523cbeaebab69c
absolutely2013 <changktc.0306@gmail.com>
2023-11-13 16:53:08 +0700

    remove LogoutButton.js , Layout.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@
 import "./App.css";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
-import { Route, Router, Routes, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React from "react";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import Layout from "./pages/Layout";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      <Layout/>
+      <Header />
       <Routes>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/register" element={<RegisterPage />}></Route>
