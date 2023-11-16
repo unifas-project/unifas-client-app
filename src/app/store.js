62e743a7702864dd30f6e2b53babfa4e49da33fb
Pham Khanh Duy <phamkhanhduy0207@gmail.com>
2023-11-16 10:32:27 +0700

    Update Header

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
-import counterReducer from '../feature/counter/counterSlice'
 import categoryReducer from '../feature/objects/categorySlice'
 import subCategoryReducer from '../feature/objects/subCategorySlice'
 
 export const store = configureStore({
     reducer:{
-        counter : counterReducer,
         category : categoryReducer,
         subCategory: subCategoryReducer
     },
