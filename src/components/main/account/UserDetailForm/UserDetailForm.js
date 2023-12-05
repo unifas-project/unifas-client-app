841c591bfc1b3eedaf1a01c3ab462f8471b7e10d
nguyentien87xd <nguyentien87xd@gmail.com>
2023-12-05 15:09:43 +0700

    FE + CONECT: view, update profile v2

--- src/components/main/account/UserDetailForm/UserDetailForm.js
+++ src/components/main/account/UserDetailForm/UserDetailForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../UserDetailForm/UserDetail.css";
+import "../../../../css/user/userdetail/UserDetail.css";
 function UserDetailForm() {
   const [user, setUser] = useState();
   const [editMode, setEditMode] = useState(false);
